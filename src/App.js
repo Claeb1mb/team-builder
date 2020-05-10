25e96e4a6ca3878f1f7706828d0392c58b360e19
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2020-05-10 04:15:00 -0400

    Wolf Pak logo and img change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./Form.js";
 
 function App() {
+  const [form, setForm] = useState([
+    {
+      Name: "Caleb",
+      Pos: "Wr, CB",
+      age: 24,
+      height: "5'10",
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <img
+          src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/69389576_666181967225936_1849252955895627776_n.jpg?_nc_cat=102&_nc_sid=110474&_nc_ohc=AWq87iDzwQ0AX-cjQqC&_nc_ht=scontent-iad3-1.xx&oh=9fb4536c9bbb3c6bf8ca517e5b0393de&oe=5EDC4504"
+          className="App-logo"
+          alt="Wolf"
+        />
+        <h1>Wolf Pak</h1>
+        <Form form={form} />
+      </div>
     </div>
   );
 }
-
 export default App;
